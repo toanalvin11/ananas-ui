fa06f272348f74d434278e0d06b4a9682edd0603
toanalvin11 <toanalvin11@gmail.com>
2022-10-12 23:19:46 +0700

    Triển khai xây dựng Header ver 1.0

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,43 @@
+import classNames from 'classnames/bind';
+import { MdLocationOn } from 'react-icons/md';
+import { BsCartFill } from 'react-icons/bs';
+import { FaHeart, FaUserAlt, FaBoxOpen } from 'react-icons/fa';
+
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('header-top')}>
+                <ul>
+                    <li>
+                        <FaBoxOpen />
+                        Tra cứu đơn hàng
+                    </li>
+                    <li>
+                        <MdLocationOn />
+                        Tìm cửa hàng
+                    </li>
+                    <li>
+                        <FaHeart />
+                        Yêu thích
+                    </li>
+                    <li>
+                        <FaUserAlt />
+                        Đăng nhập
+                    </li>
+                    <li>
+                        <BsCartFill />
+                        Giỏ hàng (0)
+                    </li>
+                </ul>
+            </div>
+            <div className={cx('header-middle')}>header-middle</div>
+            <div className={cx('header-bottom')}>header-bottom</div>
+        </header>
+    );
 }
 
 export default Header;
