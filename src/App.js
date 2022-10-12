56f329a85a00c285c3b6fdac3d00a26bc75cafbc
toanalvin11 <toanalvin11@gmail.com>
2022-10-12 18:48:03 +0700

    Reset CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-    return <div>abc</div>;
+    return (
+        <div className="App">
+            <h1>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo tempora repudiandae nisi recusandae
+                autem. Dolor inventore fuga voluptatem dicta dolore omnis sit sapiente sed porro aspernatur nobis
+                reiciendis, est delectus!
+            </h1>
+        </div>
+    );
 }
 
 export default App;
