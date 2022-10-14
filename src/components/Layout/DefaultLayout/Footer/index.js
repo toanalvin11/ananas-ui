164e6bf49770ec43f1970ea2bddf85ddad3b9975
toanalvin11 <toanalvin11@gmail.com>
2022-10-14 17:40:58 +0700

    Header

--- src/components/Layout/DefaultLayout/Footer/index.js
+++ src/components/Layout/DefaultLayout/Footer/index.js
@@ -1,5 +1,21 @@
+import classNames from 'classnames/bind';
+
+import styles from './Footer.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Footer() {
-    return <h2>Footer</h2>;
+    return (
+        <footer className={cx('wrapper')}>
+            <div className={cx('findstore')}>
+                <img src="https://ananas.vn/wp-content/themes/ananas/fe-assets/images/svg/Store.svg" alt="" />
+                <a href="#">
+                    <button>Tim cua hang</button>
+                </a>
+            </div>
+            <div className={cx('right')}>Con lai</div>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,41 +1,121 @@
 import classNames from 'classnames/bind';
 import { MdLocationOn } from 'react-icons/md';
 import { BsCartFill } from 'react-icons/bs';
+import { IoIosArrowDown, IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { FiSearch } from 'react-icons/fi';
 import { FaHeart, FaUserAlt, FaBoxOpen } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
 
 import styles from './Header.module.scss';
 
 const cx = classNames.bind(styles);
 
+const slideHotNews = [
+    {
+        caption: 'FREE SHIPPING VỚI HOÁ ĐƠN TỪ 800K !',
+        link: '#',
+    },
+    {
+        caption: 'HÀNG 2 TUẦN NHẬN ĐỔI - GIÀY NỬA NĂM BẢO HÀNH',
+        link: '#',
+    },
+    {
+        caption: 'BUY MORE PAY LESS - ÁP DỤNG KHI MUA PHỤ KIỆN',
+        link: '#',
+    },
+    {
+        caption: 'BUY 2 GET 10% OFF - ÁP DỤNG VỚI TẤT CẢ BASIC TEE',
+        link: '#',
+    },
+];
+
+const slideShow = slideHotNews.forEach((slideHotNew, index) => <div key={index}>{slideHotNew.caption}</div>);
+
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('header-top')}>
+            <div className={cx('header__top')}>
                 <ul>
                     <li>
-                        <FaBoxOpen />
-                        Tra cứu đơn hàng
+                        <a href="#">
+                            <FaBoxOpen />
+                            &ensp; Tra cứu đơn hàng
+                        </a>
                     </li>
                     <li>
-                        <MdLocationOn />
-                        Tìm cửa hàng
+                        <a href="#">
+                            <MdLocationOn />
+                            &ensp; Tìm cửa hàng
+                        </a>
                     </li>
                     <li>
-                        <FaHeart />
-                        Yêu thích
+                        <a href="#">
+                            <FaHeart />
+                            &ensp; Yêu thích
+                        </a>
                     </li>
                     <li>
-                        <FaUserAlt />
-                        Đăng nhập
+                        <a href="#">
+                            <FaUserAlt />
+                            &ensp; Đăng nhập
+                        </a>
                     </li>
                     <li>
-                        <BsCartFill />
-                        Giỏ hàng (0)
+                        <a href="#">
+                            <BsCartFill />
+                            &ensp; Giỏ hàng (0)
+                        </a>
                     </li>
                 </ul>
             </div>
-            <div className={cx('header-middle')}>header-middle</div>
-            <div className={cx('header-bottom')}>header-bottom</div>
+            <div className={cx('header-middle')}>
+                <a className={cx('header-middle__logo')}>
+                    <img src="https://ananas.vn/wp-content/themes/ananas/fe-assets/images/svg/Logo_Ananas_Header.svg" />
+                </a>
+                <div className={cx('header-middle__navbar')}>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Sản phẩm <IoIosArrowDown className={cx('arrow')} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Nam <IoIosArrowDown className={cx('arrow')} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Nữ
+                                <IoIosArrowDown className={cx('arrow')} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">Sale off</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img
+                                    src="https://ananas.vn/wp-content/themes/ananas/fe-assets/images/svg/DiscoverYOU.svg"
+                                    alt=""
+                                />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className={cx('header-middle__search')}>
+                    <button>
+                        <FiSearch />
+                    </button>
+                    <input type="text" className={cx('search')} placeholder="Tìm kiếm" />
+                </div>
+            </div>
+            <div className={cx('header__bottom')}>
+                <IoIosArrowBack />
+                <a href="#">{slideShow}Free Ship</a>
+
+                <IoIosArrowForward />
+            </div>
         </header>
     );
 }
