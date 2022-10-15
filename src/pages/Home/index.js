72e291aef8936112633ba66760ac1b33a60bb6e7
toanalvin11 <81464449+toanalvin11@users.noreply.github.com>
2022-10-15 12:38:00 +0700

    EventHotNew

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,15 @@
+import classNames from 'classnames/bind';
+
+import styles from './Home.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Home() {
-    return <h2>Home page</h2>;
+    return (
+        <div>
+            <div className={cx('abc')}>abc</div>
+        </div>
+    );
 }
 
 export default Home;
