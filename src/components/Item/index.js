dfbb9cad24e8f6d6d93633af40cb20d32ff4df6b
toanalvin11 <toanalvin11@gmail.com>
2023-03-31 23:29:43 +0700

    update header

---
+++ src/components/Item/index.js
@@ -0,0 +1,20 @@
+// function Item() {
+//     return (
+//         <div className={cx('seller-content')}>
+//             {ListBestSellers.map((ListBestSeller) => {
+//                 return (
+//                     <div className={cx('seller-card')}>
+//                         <img src={ListBestSeller.image} alt="" />
+//                         <div className={cx('seller-card-caption')}>
+//                             <strong className={cx('seller-card__title')}>{ListBestSeller.title}</strong>
+//                             <p className={cx('seller-card__color')}>{ListBestSeller.color}</p>
+//                             <strong className={cx('seller-card__price')}>{ListBestSeller.price}</strong>
+//                         </div>
+//                     </div>
+//                 );
+//             })}
+//         </div>
+//     );
+// }
+
+// export default Item;
